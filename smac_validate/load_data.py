1279bb10b8d101300e5ebe013e246b914fc878fd
KEggensperger <eggenspk@informatik.uni-freiburg.de>
2014-04-01 18:08:58 +0200

    ADD: load_data util for .csv, plot script for validated smac runs

---
+++ smac_validate/load_data.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import csv
+import os
+
+def read_csv(fn, has_header=True):
+    data = list()
+    header = None
+    with open(fn, 'rb') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csv_reader:
+            if header is None and has_header:
+                header = row
+                continue
+            data.append([i.strip() for i in row])
+    return header, data
+
+
+def get_file_and_name_list(argument_list, match_file):
+    """
+    argument_list: [<whatisthis> <file>*]*
+    match_file: string which only appears in file and not in whatisthis
+    """
+    name_list = list()
+    file_list = list()
+    no_data = False
+    for i in range(len(argument_list)):
+        if not match_file in argument_list[i] and no_data:
+            raise ValueError("You need at least one %s file per Experiment, %s has none" % (match_file, name_list[-1]))
+        elif not match_file in argument_list[i] and not no_data:
+            name_list.append(argument_list[i])
+            file_list.append(list())
+            no_data = True
+            continue
+        else:
+            if os.path.exists(argument_list[i]):
+                no_data = False
+                file_list[-1].append(os.path.abspath(argument_list[i]))
+            else:
+                raise ValueError("%s is not a valid file" % argument_list[i])
+    if no_data:
+        raise ValueError("You need at least one %s file per Experiment,  %s has none" % (match_file, name_list[-1]))
+    return file_list, name_list
---
+++ smac_validate/plot_test_of_best_train.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import sys
+import itertools
+
+from matplotlib.pyplot import tight_layout, figure, subplots_adjust, subplot, savefig, show
+import matplotlib.gridspec
+import numpy as np
+
+import load_data
+
+
+def plot_optimization_trace(times, performance_list, title, min_test, max_test, name_list,
+                            log=False, save="", y_min=0, y_max=0, x_min=0, x_max=0):
+    markers = 'o'
+    colors = itertools.cycle(["#e41a1c",    # Red
+                              "#377eb8",    # Blue
+                              "#4daf4a",    # Green
+                              "#984ea3",    # Purple
+                              "#ff7f00",    # Orange
+                              "#ffff33",    # Yellow
+                              "#a65628",    # Brown
+                              "#f781bf",    # Pink
+                              "#999999"])   # Grey
+    linestyles = '-'
+    size = 1
+
+    # Set up figure
+    ratio = 5
+    gs = matplotlib.gridspec.GridSpec(ratio, 1)
+    fig = figure(1, dpi=100)
+    fig.suptitle(title, fontsize=16)
+    ax1 = subplot(gs[0:ratio, :])
+    ax1.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+
+    # get limits
+    min_y_val = sys.maxint
+    max_y_val = -sys.maxint
+
+    for idx, performance in enumerate(performance_list):
+        color = colors.next()
+        # Plot stuff
+        ax1.plot(times, performance, color=color, linewidth=size,
+                 linestyle=linestyles, marker=markers, label=name_list[idx])
+        ax1.fill_between(times, min_test[idx], max_test[idx], facecolor=color, alpha=0.2, edgecolor="")
+        # Get limits
+        min_y_val = min(min(min_test[idx]), min_y_val)
+        max_y_val = max(max(max_test[idx]), max_y_val)
+    max_trials = times[-1]
+
+    # Label axes
+    if log:
+        ax1.set_ylabel("log10(Performance)")
+    else:
+        ax1.set_ylabel("Performance")
+
+    ax1.set_xlabel("log10(time) [sec]")
+
+    # Set axes limit
+    ax1.set_xscale("log")
+
+    if y_max == y_min:
+        ax1.set_ylim([min_y_val-0.1*abs((max_y_val-min_y_val)), max_y_val+0.1*abs((max_y_val-min_y_val))])
+    else:
+        # User has predefined limits
+        ax1.set_ylim([y_min, y_max])
+
+    x_min = min(0, x_min)
+    if x_max == 0 and x_min >= 0:
+        ax1.set_xlim([x_min, max_trials + 0.01*abs(max_trials)])
+    elif x_max != 0 and x_min == 0:
+        ax1.set_xlim([0, x_max])
+    elif x_max > x_min and x_min <= 0:
+        ax1.set_xlim([x_min, x_max])
+    else:
+        ax1.set_xlim([0, max_trials + 0.01*abs(max_trials)])
+
+    leg = ax1.legend(loc='best', fancybox=True)
+    leg.get_frame().set_alpha(0.5)
+
+    # Save or show figure
+    tight_layout()
+    subplots_adjust(top=0.85)
+    if save != "":
+        savefig(save, dpi=100, facecolor='w', edgecolor='w',
+                orientation='portrait', papertype=None, format=None,
+                transparent=False, pad_inches=0.1)
+    else:
+        show()
+
+
+def get_test_of_best_train(train, test):
+    # get argmin of test performance and return test performance at this idx
+    idx = np.argmin(train, 0)
+    return [test[idx[row], row] for row in range(len(idx))]
+
+
+def main():
+    prog = "python plot_test_of_best_train.py <path_to_ClassicValidationResults*.csv>"
+    description = "Plot a test error of best training trial"
+
+    parser = ArgumentParser(description=description, prog=prog)
+
+    # General Options
+    parser.add_argument("-l", "--log", action="store_true", dest="log",
+                        default=False, help="Plot on log scale")
+    parser.add_argument("--ymax", dest="ymax", type=float,
+                        default=0, help="Maximum of the y-axis")
+    parser.add_argument("--ymin", dest="ymin", type=float,
+                        default=0, help="Minimum of the y-axis")
+    parser.add_argument("--xmax", dest="xmax", type=float,
+                        default=0, help="Maximum of the x-axis")
+    parser.add_argument("--xmin", dest="xmin", type=float,
+                        default=0, help="Minimum of the x-axis")
+    parser.add_argument("-s", "--save", dest="save",
+                        default="", help="Where to save plot instead of showing it?")
+    parser.add_argument("-t", "--title", dest="title",
+                        default="", help="Optional supertitle for plot")
+    parser.add_argument("--maxvalue", dest="maxvalue", type=float,
+                        default=sys.maxint, help="Replace all values higher than this?")
+
+    args, unknown = parser.parse_known_args()
+
+    if len(unknown) < 2:
+        print "To less arguments given"
+        print(parser.usage)
+        sys.exit(1)
+
+    sys.stdout.write("Found " + str(len(unknown)) + " arguments\n")
+
+    # Get files and names
+    file_list, name_list = load_data.get_file_and_name_list(unknown, match_file='.csv')
+    for idx in range(len(name_list)):
+        print "%20s contains %d file(s)" % (name_list[idx], len(file_list[idx]))
+
+    # Get data from csv
+    train_performance = list()
+    test_performance = list()
+    time_ = list()
+    for name in range(len(name_list)):
+        # We have a new experiment
+        train_performance.append(list())
+        test_performance.append(list())
+        for fl in file_list[name]:
+            _none, csv_data = load_data.read_csv(fl, has_header=False)
+            csv_data = np.array(csv_data)
+            # Replace too high values with args.maxint
+            train_performance[-1].append([min([args.maxvalue, float(i.strip())]) for i in csv_data[:, 1]])
+            test_performance[-1].append([min([args.maxvalue, float(i.strip())]) for i in csv_data[:, 2]])
+            time_.append([float(i.strip()) for i in csv_data[:, 0]])
+        # Check whether we have the same times for all runs
+        if len(time_) == 2:
+            if time_[0] == time_[1]:
+                time_ = [time_[0], ]
+            else:
+                raise NotImplementedError(".csv are not using the same times")
+    train_performance = [np.array(i) for i in train_performance]
+    test_performance = [np.array(i) for i in test_performance]
+    time_ = np.array(time_).flatten()
+
+    # Now get the test results for the best train performance
+    # All arrays are numExp x numTrials
+    test_of_best_train = list()
+    min_test = list()
+    max_test = list()
+    for i in range(len(train_performance)):
+        test_of_best_train.append(get_test_of_best_train(train_performance[i], test_performance[i]))
+        min_test.append(np.min(test_performance[i], 0))
+        max_test.append(np.max(test_performance[i], 0))
+
+    save = ""
+    if args.save != "":
+        save = args.save
+        print "Save to %s" % args.save
+    else:
+        print "Show"
+    plot_optimization_trace(times=time_, performance_list=test_of_best_train, min_test=min_test, max_test=max_test,
+                            name_list=name_list, title=args.title, log=args.log, save=save, y_min=args.ymin,
+                            y_max=args.ymax, x_min=args.xmin, x_max=args.xmax)
+
+if __name__ == "__main__":
+    main()

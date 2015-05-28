613fec8f37da35e9138f98beaa15b4b15c197e78
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2015-05-28 15:32:46 +0200

    make it possible to have more than one name to describe dataset

--- smac_validate/load_data.py
+++ smac_validate/load_data.py
@@ -17,28 +17,40 @@ def read_csv(fn, has_header=True, data_type=str):
     return header, data
 
 
-def get_file_and_name_list(argument_list, match_file):
+def get_file_and_name_list(argument_list, match_file, len_name=1):
     """
     argument_list: [<whatisthis> <file>*]*
     match_file: string which only appears in file and not in whatisthis
+    len_name: len of names describing file(s) (if >1 return list of tuples)
     """
+    assert 0 < len_name == int(len_name)
     name_list = list()
     file_list = list()
-    no_data = False
+    len_desc = 0
     for i in range(len(argument_list)):
-        if not match_file in argument_list[i] and no_data:
+        if not match_file in argument_list[i] and len_desc == len_name:
+            # We have all names, but next argument is not a file
             raise ValueError("You need at least one %s file per Experiment, %s has none" % (match_file, name_list[-1]))
-        elif not match_file in argument_list[i] and not no_data:
-            name_list.append(argument_list[i])
-            file_list.append(list())
-            no_data = True
+        elif not match_file in argument_list[i] and len_desc < len_name:
+            # We start with a new name desc
+            if len_name > 1 and len_desc == 0:
+                name_list.append(list([argument_list[i], ]))
+            elif len_name > 1 and len_desc > 0:
+                name_list[-1].append(argument_list[i])
+            else:
+                name_list.append(argument_list[i])
+
+            len_desc += 1
+
+            if len_desc == len_name:
+                # We have all desc for this file
+                file_list.append(list())
             continue
         else:
             if os.path.exists(argument_list[i]):
-                no_data = False
+                len_desc = 0
                 file_list[-1].append(os.path.abspath(argument_list[i]))
             else:
                 raise ValueError("%s is not a valid file" % argument_list[i])
-    if no_data:
-        raise ValueError("You need at least one %s file per Experiment,  %s has none" % (match_file, name_list[-1]))
+
     return file_list, name_list

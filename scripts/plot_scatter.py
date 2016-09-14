b5d4421a50b7650764e54f7dabcf2a42581c400b
Katharina Eggensperger <eggenspk@informatik.uni-freiburg.de>
2016-09-14 16:58:51 +0200

    python3

--- scripts/plot_scatter.py
+++ scripts/plot_scatter.py
@@ -11,32 +11,34 @@ import plottingscripts.plotting.scatter as scatter
 
 def main():
     prog = "python plot_scatter.py"
-    description = "Plots performances of the best config at one time vs another in a scatter plot"
+    description = "Plots performances of the best config at one time vs " \
+                  "another in a scatter plot"
 
     parser = ArgumentParser(description=description, prog=prog)
 
     # General Options
-    # parser.add_argument("-l", "--log", action="store_true", dest="log",
-    #                     default=False, help="Plot on log scale")
     parser.add_argument("--max", dest="max", type=float,
                         default=1000, help="Maximum of both axes")
     parser.add_argument("--min", dest="min", type=float,
                         default=None, help="Minimum of both axes")
     parser.add_argument("-s", "--save", dest="save",
-                        default="", help="Where to save plot instead of showing it?")
+                        default="", help="Where to save plot instead of "
+                                         "showing it?")
     parser.add_argument("--title", dest="title",
                         default="", help="Optional supertitle for plot")
     parser.add_argument("--greyFactor", dest="grey_factor", type=float,
-                        default=1, help="If an algorithms is not greyFactor-times better"
-                                        " than the other, show this point less salient, > 1")
-    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False,
-                        help="Plot some debug info")
+                        default=1, help="If an algorithms is not greyFactor-"
+                                        "times better than the other, show this"
+                                        " point less salient, > 1")
+    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true",
+                        default=False, help="Plot some debug info")
     parser.add_argument("-f", "--lineFactors", dest="linefactors",
-                        default=None, help="Plot X speedup/slowdown, format 'X,..,X' (no spaces)")
+                        default=None, help="Plot X speedup/slowdown,"
+                                           " format 'X,..,X' (no spaces)")
     parser.add_argument("--time", dest="time", default=None,
-                        help="Plot config at which time?, format 'time1,time2' ")
+                        help="Plot config at which time?, format 'time1,time2'")
     parser.add_argument("--obj", dest="obj", default=None, required=True,
-                        help="Path to validationObjectiveMatrix-traj-run-* file")
+                        help="Path to validationObjectiveMatrix-traj-* file")
     parser.add_argument("--res", dest="res", required=True,
                         help="Path to validationResults-traj-run-* file")
     parser.add_argument("--minvalue", dest="minvalue", type=float,
@@ -47,38 +49,42 @@ def main():
     args, unknown = parser.parse_known_args()
 
     if len(unknown) != 0:
-        print "Wrong number of arguments"
+        print("Wrong number of arguments")
         parser.print_help()
         sys.exit(1)
 
     if args.grey_factor < 1:
-        print "A grey-factor lower than one makes no sense"
+        print("A grey-factor lower than one makes no sense")
         parser.print_help()
         sys.exit(1)
 
     # Load validationResults
     res_header, res_data = read_util.read_csv(args.res, has_header=True)
     av_times = [float(row[0]) for row in res_data]
-    if args.time == None:
+    if args.time is None:
         # Print available times and quit
-        print "Choose a time from"
-        print "\n".join(["* %s" % i for i in av_times])
+        print("Choose a time from")
+        print("\n".join(["* %s" % i for i in av_times]))
         sys.exit(0)
     time_arr = args.time.split(",")
-    if len(time_arr) != 2 or (len(time_arr) == 2 and (time_arr[1] == "" or time_arr[0] == "")):
-        print "Something wrong with %s, should be 'a,b'" % args.time
-        print "Choose a time from"
-        print "\n".join(["* %s" % i for i in av_times])
+    if len(time_arr) != 2 or \
+            (len(time_arr) == 2 and (time_arr[1] == "" or time_arr[0] == "")):
+        print("Something wrong with %s, should be 'a,b'" % args.time)
+        print("Choose a time from")
+        print("\n".join(["* %s" % i for i in av_times]))
         sys.exit(0)
     time_1 = float(time_arr[0])
     time_2 = float(time_arr[1])
 
     # Now extract data
-    config_1 = [int(float(row[len(res_header)-2].strip('"'))) for row in res_data if int(float(row[0])) == int(time_1)]
-    config_2 = [int(float(row[len(res_header)-2].strip('"'))) for row in res_data if int(float(row[0])) == int(time_2)]
+    config_1 = [int(float(row[len(res_header)-2].strip('"'))) for row in
+                res_data if int(float(row[0])) == int(time_1)]
+    config_2 = [int(float(row[len(res_header)-2].strip('"'))) for row in
+                res_data if int(float(row[0])) == int(time_2)]
     if len(config_1) == 0 or len(config_2) == 0:
-        print "Time int(%s) or int(%s) not found. Choose a time from:" % (time_1, time_2)
-        print "\n".join(["* %s" % i for i in av_times])
+        print("Time int(%s) or int(%s) not found. Choose a time from:" %
+              (time_1, time_2))
+        print("\n".join(["* %s" % i for i in av_times]))
         sys.exit(1)
     config_1 = config_1[0]
     config_2 = config_2[0]
@@ -91,16 +97,17 @@ def main():
     data_one = np.array([float(row[idx_1].strip('"')) for row in obj_data])
     data_two = np.array([float(row[idx_2].strip('"')) for row in obj_data])
 
-    print "Found %s points for config %d and %s points for config %d" % (str(data_one.shape), config_1, str(data_two.shape), config_2)
+    print("Found %s points for config %d and %s points for config %d" %
+          (str(data_one.shape), config_1, str(data_two.shape), config_2))
 
     linefactors = list()
     if args.linefactors is not None:
         linefactors = [float(i) for i in args.linefactors.split(",")]
         if len(linefactors) < 1:
-            print "Something is wrong with linefactors: %s" % args.linefactors
+            print("Something is wrong with linefactors: %s" % args.linefactors)
             sys.exit(1)
         if min(linefactors) < 1:
-            print "A line-factor lower than one makes no sense"
+            print("A line-factor lower than one makes no sense")
             sys.exit(1)
     if args.grey_factor > 1 and args.grey_factor not in linefactors:
         linefactors.append(args.grey_factor)
@@ -116,8 +123,10 @@ def main():
     data_two = np.array([max(args.minvalue, i) for i in data_two])
 
     fig = scatter.plot_scatter_plot(x_data=data_one, y_data=data_two,
-                                    labels=[label_template % (config_1, str(time_1)),
-                                            label_template % (config_2, str(time_2))],
+                                    labels=[label_template %
+                                            (config_1, str(time_1)),
+                                            label_template %
+                                            (config_2, str(time_2))],
                                     title=args.title,
                                     max_val=args.max, min_val=args.min,
                                     grey_factor=args.grey_factor,
@@ -125,7 +134,7 @@ def main():
                                     user_fontsize=args.fontsize,
                                     debug=args.verbose)
     if args.save != "":
-        print "Save plot to %s" % args.save
+        print("Save plot to %s" % args.save)
         plot_util.save_plot(fig, args.save, plot_util.get_defaults()['dpi'])
     else:
         fig.show()
